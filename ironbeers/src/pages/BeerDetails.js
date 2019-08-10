4f728eb55ae0de58ace81a2009e80eaf15e10d89
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-08-10 14:54:31 +0200

    comment out beer detail

--- ironbeers/src/pages/BeerDetails.js
+++ ironbeers/src/pages/BeerDetails.js
@@ -1,36 +1,36 @@
-import React, {Component} from 'react';
-import Navbar from '../components/Navbar';
-import axios from 'axios';
+// import React, {Component} from 'react';
+// import Navbar from '../components/Navbar';
+// import axios from 'axios';
 
-class BeerDetails extends Component {
+// class BeerDetails extends Component {
 
-  state = {
-    beer: null
-  }
+//   state = {
+//     beer: null
+//   }
 
-  componentDidMount() {
-    const {id} = this.props.match.params;
-    axios.get(`https://ih-beer-api.herokuapp.com/beers/${beer.id}`)
-    .then((response) => {
-      console.log(response) // ???
-      // this.setState({
-      //   beer: response.
-      // })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
+//   componentDidMount() {
+//     const {id} = this.props.match.params;
+//     axios.get(`https://ih-beer-api.herokuapp.com/beers/${beer.id}`)
+//     .then((response) => {
+//       console.log(response) // ???
+//       // this.setState({
+//       //   beer: response.
+//       // })
+//     })
+//     .catch((error) => {
+//       console.log(error)
+//     })
+//   }
 
-  render() {
-    return (
-      <>
-        <Navbar />
-        <h2>Beer Details Page</h2>
-      </>
-    )
-  }
+//   render() {
+//     return (
+//       <>
+//         <Navbar />
+//         <h2>Beer Details Page</h2>
+//       </>
+//     )
+//   }
 
-}
+// }
 
-export default BeerDetails;
+// export default BeerDetails;
